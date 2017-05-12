16511c3602a684c02ca46ed15ded5aa2aeac3abc
Zachary Sailer <zachsailer@gmail.com>
2017-05-11 17:01:05 -0700

    ready for initial figures

--- generated-data/download.sh
+++ generated-data/download.sh
@@ -1,5 +1,5 @@
 
-scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-script/full-state-predictions/results/* full-state-predictions/results/
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/* full-state-predictions/results/
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/* one-state-predictions/results/
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/* two-state-predictions/results/
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/* three-state-predictions/results/
