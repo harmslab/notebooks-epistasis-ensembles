e28244d45010bb8e067a1d3a4b86736bae4418a1
ZSailer <zachsailer@gmail.com>
2017-05-24 15:53:20 -0700

    temporary noodling notebooks for version control

--- generated-data/download.sh
+++ generated-data/download.sh
@@ -1,6 +1,16 @@
 
-scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/* full-state-predictions/results/
-scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/* one-state-predictions/results/
-scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/* two-state-predictions/results/
-scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/* three-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/net* full-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/net* one-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/net* two-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/net* three-state-predictions/results/
 
+
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/statistics-output.pickle full-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/statistics-output.pickle one-state-predictions/results/
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/statistics-output.pickle two-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/statistics-output.pickle three-state-predictions/results/
+
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/binary* full-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/binary* one-state-predictions/results/
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/binary* two-state-predictions/results/
+#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/binary* three-state-predictions/results/
