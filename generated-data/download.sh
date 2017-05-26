c2bd94897904a82a5e3fd62e1297d82a863c0fe3
Zachary Sailer <zachsailer@gmail.com>
2017-05-26 07:55:02 -0700

    adding new notebooks figures

--- generated-data/download.sh
+++ generated-data/download.sh
@@ -8,9 +8,9 @@
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/statistics-output.pickle full-state-predictions/results/
 #scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/statistics-output.pickle one-state-predictions/results/
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/statistics-output.pickle two-state-predictions/results/
-#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/statistics-output.pickle three-state-predictions/results/
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/statistics-output.pickle three-state-predictions/results/
 
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/full-state-predictions/results/binary* full-state-predictions/results/
 #scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/one-state-predictions/results/binary* one-state-predictions/results/
 scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/two-state-predictions/results/binary* two-state-predictions/results/
-#scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/binary* three-state-predictions/results/
+scp zsailer@ftn.aciss.uoregon.edu:epistasis-ensembles-scripts/three-state-predictions/results/binary* three-state-predictions/results/
