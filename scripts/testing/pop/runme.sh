64828cf6643e73a7605c6e2823da33deaae9ea8f
Zachary Sailer <zachsailer@gmail.com>
2017-08-29 16:34:00 -0700

    working out population size problem

--- scripts/testing/pop/runme.sh
+++ scripts/testing/pop/runme.sh
@@ -1,9 +1,23 @@
-qsub runme1.pbs
-qsub runme2.pbs
-qsub runme3.pbs
-qsub runme4.pbs
-qsub runme5.pbs
-qsub runme6.pbs
-qsub runme7.pbs
-qsub runme8.pbs
-qsub runme9.pbs
+qsub runme.pbs -v POPSIZE=1,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+qsub runme.pbs -v POPSIZE=10,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+qsub runme.pbs -v POPSIZE=100,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+qsub runme.pbs -v POPSIZE=1000,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+qsub runme.pbs -v POPSIZE=10000,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+qsub runme.pbs -v POPSIZE=100000,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+qsub runme.pbs -v POPSIZE=1000000,N_MOVES=7,N_BRANCHES=3,N_STATES=3
+
+qsub runme.pbs -v POPSIZE=1,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+qsub runme.pbs -v POPSIZE=10,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+qsub runme.pbs -v POPSIZE=100,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+qsub runme.pbs -v POPSIZE=1000,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+qsub runme.pbs -v POPSIZE=10000,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+qsub runme.pbs -v POPSIZE=100000,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+qsub runme.pbs -v POPSIZE=1000000,N_MOVES=7,N_BRANCHES=3,N_STATES=0 
+
+qsub runme.pbs -v POPSIZE=1,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
+qsub runme.pbs -v POPSIZE=10,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
+qsub runme.pbs -v POPSIZE=100,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
+qsub runme.pbs -v POPSIZE=1000,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
+qsub runme.pbs -v POPSIZE=10000,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
+qsub runme.pbs -v POPSIZE=100000,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
+qsub runme.pbs -v POPSIZE=1000000,N_MOVES=7,N_BRANCHES=3,N_STATES=2 
--- scripts/testing/readme.md
+++ scripts/testing/readme.md
@@ -8,3 +8,9 @@
     2. Get the sequences and fixation probabilites
 2. Sample those trajectories with different popsize
 3. Compare true trajectories (probabilities from actual lattice proteins) to predicted trajectories (using 2nd-order epistasis model)
+
+We set out to determine whether population size for our predictions. In smaller populations,
+selection has a smaller effect on the fixation probability, and therefore genetic drift plays
+a major role in determining trajectories. Conversely, in larger population sizes, selection
+dominates. In our simulations, we probe the extreme case of infinite population size. In this
+regime, we would expect our predictive power is strongest. 
